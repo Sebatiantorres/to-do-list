13882d8de1e708945cea0b84524d82bfe7f62cee
Javier Diaz <javierdiazt406@gmail.com>
2024-08-06 22:59:07 -0400

    CreateTask component and Show Task list Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Card from './components/Card'
 import CardButtons from './components/CardButtons'
 import CreateTasks from './components/CreateTasks'
@@ -6,18 +7,30 @@ import './styles/App.css'
 
 
 function App() {
+  const [tasks, setTasks] = useState<string[]>([])
+
   return (
     <>
       <Navbar />
       <Card>
-      <div className="card-content">
-        <CreateTasks />
-        <div className='card-title'>
-          <h2>Card title</h2>
+        <div className="card-content">
+          <div className='card-title'>
+            <p>Create, manage and remove all To Do List tasks below</p>
+          </div>
+          <CardButtons />
         </div>
-        <CardButtons />
-      </div>
-      <p>Card content</p>
+        <CreateTasks
+          onCreate={(task: string) => {
+            setTasks([...tasks, task])
+          }}
+        />
+        <div className='task-list'>
+          {tasks.map((task, index) => (
+            <div key={index} className='task'>
+              <p>{task}</p>
+            </div>
+          ))}
+        </div>
       </Card>
     </>
   )
